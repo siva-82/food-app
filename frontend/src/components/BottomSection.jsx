00e1d6c776199e136a1d2bab407e4817e14cc03b
siva-82 <iamsiva1702@gmail.com>
2024-06-13 16:16:27 +0530

    add navigation

--- frontend/src/components/BottomSection.jsx
+++ frontend/src/components/BottomSection.jsx
@@ -7,7 +7,7 @@ const BottomSection = () => {
 <div className='container-fluid bottomSection  d-flex flex-column justify-content-center align-items-center' style={{height:'300px',backgroundImage:'url(\'/bottomSection.png\')',backgroundRepeat:'round' }}>
 <div className='bottomSection d-flex flex-column justify-content-center align-items-center '>
     <h1 style={{width:'550px',textAlign:'center',color:'white'}}>Are you ready to order with the beast deals?</h1>
-    <button className='my-5 btn btn-primary' style={{padding:'15px',backgroundColor: "rgb(241, 114, 40)", borderColor: "rgb(241, 114, 40)"}}>PROCEED TO ORDER <span><MdOutlineKeyboardArrowRight/></span></button>
+    <button className='my-5 btn btn-primary' style={{padding:'15px',backgroundColor: "rgb(241, 114, 40)", borderColor: "rgb(241, 114, 40)"}} onClick={()=>navigate('/Food')}>PROCEED TO ORDER <span><MdOutlineKeyboardArrowRight/></span></button>
 </div>
 
     </div>
