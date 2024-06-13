916c1c576b18e8d796e5696f49e4f85e12454e43
siva-82 <iamsiva1702@gmail.com>
2024-06-13 18:03:00 +0530

    url

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -27,7 +27,7 @@ const NavBar = () => {
   const checkout = async () => {
     console.log("checkout")
 setCheckOutLoading(true)
-    await fetch(`${process.env.REACT_APP_BACKEND_URL}/checkout`, {
+    await fetch(`https://food-app-37dd.onrender.com/checkout`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
